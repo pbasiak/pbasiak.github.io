56dd948317df6289a7cd32c737789bde87abb8d4
pbasiak <pawel.basiak01@gmail.com>
2015-08-21 17:44:11 +0200

    Update styli + nowe realizacje + nowa sekcja na froncie

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,14 +1,30 @@
 $(document).ready(function() {
-	console.log('Dziala');
-	var movementStrength = 25;
-	var height = movementStrength / $(window).height();
-	var width = movementStrength / $(window).width();
-	$(".main-page").mousemove(function(e){
-	          var pageX = e.pageX - ($(window).width() / 2);
-	          var pageY = e.pageY - ($(window).height() / 2);
-	          var newvalueX = width * pageX * -1 - 25;
-	          var newvalueY = height * pageY * -1 - 50;
-	          $('.front-header').css("background-position", newvalueX+"px     "+newvalueY+"px");
+	console.log('Main.js załadowany.');
+	if ( (window).width < 1024 ) {
+		var movementStrength = 25;
+		var height = movementStrength / $(window).height();
+		var width = movementStrength / $(window).width();
+		$(".main-page").mousemove(function(e){
+		          var pageX = e.pageX - ($(window).width() / 2);
+		          var pageY = e.pageY - ($(window).height() / 2);
+		          var newvalueX = width * pageX * -1 - 25;
+		          var newvalueY = height * pageY * -1 - 50;
+		          $('.front-header').css("background-position", newvalueX+"px     "+newvalueY+"px");
+		});
+	}
+
+	/* Skills */
+
+	console.log('Jestem');
+
+	$('.skills-item .skill-wrapper').each(function(){
+		var skillWidth = $(this).children('span').attr('data-width');
+
+		$(this).children('span').css('width', skillWidth);
+
+		$(this).find('.tooltip').text(skillWidth);
+
+		$(this).find('.tooltip').css('left', skillWidth);
 	});
 });
 
