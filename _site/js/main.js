3579d937344ef5c04cae7c3b15367f9028e84451
pbasiak <pawel.basiak01@gmail.com>
2016-06-10 13:22:43 +0200

    Up new

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,21 +1,5 @@
 $(document).ready(function() {
-	console.log('Main.js załadowany.');
-	if ( (window).width < 1024 ) {
-		var movementStrength = 25;
-		var height = movementStrength / $(window).height();
-		var width = movementStrength / $(window).width();
-		$(".main-page").mousemove(function(e){
-		          var pageX = e.pageX - ($(window).width() / 2);
-		          var pageY = e.pageY - ($(window).height() / 2);
-		          var newvalueX = width * pageX * -1 - 25;
-		          var newvalueY = height * pageY * -1 - 50;
-		          $('.front-header').css("background-position", newvalueX+"px     "+newvalueY+"px");
-		});
-	}
-
 	/* Skills */
-
-	console.log('Jestem');
 
 	$('.skills-item .skill-wrapper').each(function(){
 		var skillWidth = $(this).children('span').attr('data-width');
